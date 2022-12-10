881768601d245a92ce332fcdab424e6ea0c67a79
Nabila <nabilabasya.business@gmail.com>
2022-12-10 20:51:52 +0700

    Task crud education

---
+++ src/main/java/com/example/demo/request/SaveEducationRequest.java
@@ -0,0 +1,9 @@
+package com.example.demo.request;
+
+import lombok.Getter;
+
+@Getter
+public class SaveEducationRequest {
+    private String name;
+    private String description;
+}
---
+++ src/main/java/com/example/demo/request/UpdateEducationRequest.java
@@ -0,0 +1,9 @@
+package com.example.demo.request;
+
+import lombok.Getter;
+
+@Getter
+public class UpdateEducationRequest {
+    private String name;
+    private String description;
+}
